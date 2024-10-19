6199747c7816bd9f70f8c6521477117c90908641
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-10-19 01:00:32 +0100

    add Spiral Matrix II solution

---
+++ 59. Spiral Matrix II/59. Spiral Matrix II.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int top = 0;
+        int bottom = n-1;
+        int right = n-1;
+        int left = 0 ;
+        int i =1;
+        while (i<=Math.pow(n,2)) {
+            for(int j=left ; j<=right ; j++){
+                result[top][j] = i++;
+            }
+            top++;
+
+            for(int j=top ; j<=bottom; j++){
+                result[j][right] = i++;
+            }
+            right--;
+
+            for(int j=right ; j>=left; j--){
+                result[bottom][j] = i++;
+            }
+            bottom--;
+            for(int j=bottom; j>=top ; j--){
+                result[j][left]= i++;
+            }
+            left++;
+
+        }
+        return result;
+        
+    }
+}
