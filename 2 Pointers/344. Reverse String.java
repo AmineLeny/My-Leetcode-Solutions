1455d51aacd4e18d488043ecd783878bc093599c
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-10-23 17:49:29 +0100

    add new problem

---
+++ 2 Pointers/344. Reverse String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int j = n-1;
+        char temp ;
+        for (int i =0 ; i < j; ++i) {
+            temp = s[i];
+            s[i]=s[j];
+            s[j]= temp;
+            j--;             
+        }
+    }
+}
