585d360d6bf7e335b5c77300d038802aa885e7e1
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-11-29 02:56:50 +0100

    Add increasing Triplet Subsequence problem
    Greedy Approach

---
+++ Arrays & Strings/334. Increasing Triplet Subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int f=Integer.MAX_VALUE,s= Integer.MAX_VALUE;
+
+        for(int i : nums) { 
+            if(f>=i) f=i;
+            else if (s>=i) s=i;
+            else {
+                return true;
+            }
+        }
+        return false;
+       
+    
+    }}
