c1f6820aca9d68aa732a5b52d6e7c983daa277b3
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-11-30 21:11:53 +0100

    add dp problem

---
+++ Dynamic Programming/70. Climbing Stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int a =0;
+        int b =1;
+      
+        for(int i =0 ; i<n ; ++i) { 
+            b = a+b ; 
+            a = b-a;
+        }
+       return b;
+    }
+}
