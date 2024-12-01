ed1a2e4db754fd78feaaa25556649ceb7f029c0d
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-12-02 00:47:06 +0100

    add new generate parenthesis solution

--- Stack/22. Generate Parentheses.java
+++ Stack/22. Generate Parentheses.java
@@ -10,3 +10,27 @@ class Solution {
         if(s<f) backtrack( result, r+")" , n , f, s+1);
     }
 }
+
+
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>(); 
+        StringBuilder b = new StringBuilder();
+        backtrack(result,b,n,0,0);
+        return result;
+    }
+    public void backtrack(List<String> result, StringBuilder b,int n , int f , int s) {
+        if (f==n && s==n) result.add(b.toString());
+        if(f<n) {
+        b.append("(");
+        backtrack( result, b , n , f+1, s);
+        b.deleteCharAt(b.length()-1);
+        }
+        if(s<f) {
+        b.append(")");
+        backtrack( result, b , n , f, s+1);
+        b.deleteCharAt(b.length()-1);
+        }
+    }
+}
