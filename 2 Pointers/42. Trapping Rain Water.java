2c3828eef4360735c6f5ea7715b68ff08d634f48
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-12-02 01:08:55 +0100

    add slightly improved solution to .42

--- 2 Pointers/42. Trapping Rain Water.java
+++ 2 Pointers/42. Trapping Rain Water.java
@@ -26,3 +26,27 @@ class Solution {
         
     }
 }
+
+
+
+class Solution {
+    public int trap(int[] height) {
+      int result = 0; 
+      int n = height.length;
+    int[] maxRight = new int[n];
+    int maxLeft = 0;
+    maxRight[n-1] = 0;
+     for( int i=n-2 ; i>-1 ; --i ){
+        maxRight[i] = Math.max(maxRight[i+1],height[i+1]);
+     }
+     for(int i =0 ; i<n ; i++ ){
+        if( Math.min(maxLeft,maxRight[i])-height[i]>0){ 
+            result += Math.min(maxLeft,maxRight[i])-height[i];
+        }
+        maxLeft = Math.max(maxLeft,height[i]);
+     }
+
+     return result;
+        
+    }
+}
