60d39e2ee886a75195056bfa602eb82521c2fbe2
Laanaya Amine <72925780+AmineLeny@users.noreply.github.com>
2025-07-23 23:55:13 +0100

    [235] [Time Beats: 47.59%] [Memory Beats: 5.48%] - LeetPush

--- Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -9,30 +9,14 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if root is None:
             return None
+        
+        if p.val < root.val and q.val<root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        return root
 
-        def bfs(root, l,target):
-            if root is None:
-                return []
-            l.append(root)
-            if root.val == target:
-                return l[:]
-            right =bfs(root.right,l,target)
-            left = bfs(root.left,l,target)
-            if right:
-                return right
-            if left :
-                return left
-            l.pop()
-            
-
-        plist = bfs(root,[],p.val)
-        qlist = bfs(root,[],q.val)
-    
-        for i in range(len(plist)-1,-1,-1):
-            if plist[i] in qlist:
-                return plist[i]
-        return None
-                
+        
                             
             
             
