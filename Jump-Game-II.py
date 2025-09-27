35864eae8f112ccd79ab18d4fe09a7231d604628
Laanaya Amine <72925780+AmineLeny@users.noreply.github.com>
2025-09-27 19:04:53 +0100

    [45] [Time Beats: 43.67%] [Memory Beats: 20.46%] - LeetPush

--- Jump-Game-II.py
+++ Jump-Game-II.py
@@ -2,12 +2,14 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         far = 0
         end = 0
-        min_jumps =0
-        for i,v in enumerate(nums) : 
-            if i==len(nums)-1: break
-            far = max(far,v+i)
+        jumps =0
+        for i in range(len(nums)-1) : 
+            far = max(far,nums[i]+i)
+
             if i == end :
-                min_jumps +=1
-                end = far
-        return min_jumps
+                jumps+=1 
+                end =far
+                
+        return jumps
+            
             
